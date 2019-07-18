7bb8f1a9de4638622c912d75cf5d75d38c35a298
PatrickKann <52541434+PatrickKann@users.noreply.github.com>
2019-07-18 12:09:46 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,28 @@
+import { Song } from '../../app/song';
+
+export const SONGS: Song[] = [
+  { songName: 'Atlas, Rise!',
+    artist: 'Metallica',
+    genre: 'Rock n\' Roll',
+    year: 2016,
+    picture: 'assets/image/hardwired.jpg'
+  },
+  { songName: 'Seek and Destroy',
+    artist: 'Metallica',
+    genre: 'Rock n\' Roll',
+    year: 1983,
+    picture: 'assets/image/killemall.jpg'
+  },
+  { songName: 'Master Of Puppets',
+    artist: 'Metallica',
+    genre: 'Rock n\' Roll',
+    year: 1986,
+    picture: 'assets/image/masterofpuppets.jpg'
+  },
+  { songName: 'Wherever I May Roam',
+    artist: 'Metallica',
+    genre: 'Rock n\' Roll',
+    year: 2013,
+    picture: 'assets/image/throughthenever.jpg'
+  }
+];
